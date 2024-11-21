54449cf63ab3c23652f736d9d58aaea1f391bbb7
Siddhant Verma <siddhantverma@Siddhants-MacBook-Pro.local>
2024-11-22 02:37:02 +0900

    Skeleton

---
+++ Springlearn/src/test/java/com/xverma/Springlearn/SpringlearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xverma.Springlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringlearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
