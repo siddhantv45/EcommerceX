acb8621b07cb732fcdd4fac75e7eeb1c722b1890
Siddhant Verma <siddhantverma@Siddhants-MacBook-Pro.local>
2024-11-26 21:41:09 +0900

    added lombok

--- Springlearn/src/main/java/com/xverma/Springlearn/model/Category.java
+++ Springlearn/src/main/java/com/xverma/Springlearn/model/Category.java
@@ -4,33 +4,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import org.springframework.boot.autoconfigure.web.WebProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
     private String categoryName;
-
-    public Category(Long categoryId, String categoryName) {
-        this.categoryId = categoryId;
-        this.categoryName = categoryName;
-    }
-
-    public void setCategoryId(Long categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
-
-    public Long getCategoryId() {
-        return categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
 }
--- Springlearn/src/main/java/com/xverma/Springlearn/repository/CategoryRepository.java
+++ Springlearn/src/main/java/com/xverma/Springlearn/repository/CategoryRepository.java
@@ -2,7 +2,6 @@ package com.xverma.Springlearn.repository;
 
 import com.xverma.Springlearn.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
 public interface CategoryRepository extends JpaRepository<Category,Long> {
 }
